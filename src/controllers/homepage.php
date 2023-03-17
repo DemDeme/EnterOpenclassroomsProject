a2b003b4deeb0628db5f11d5b3a62a4094301ea4
Mehdi ALA <mehdiala59@gmail.com>
2023-03-17 11:21:15 +0100

    retry repair

--- src/controllers/homepage.php
+++ src/controllers/homepage.php
@@ -3,18 +3,29 @@
 namespace Application\Controllers\Homepage;
 
 require_once('src/model/active.php');
+require_once('src/lib/database.php');
+require_once('src/model/plant.php');
 
 use Application\Model\Active\Active;
+use Application\Lib\Database\DatabaseConnection;
+use Application\Model\Plant\LastPosts;
+use Application\Model\Plant\PlantSheet;
 
 
 
 class Homepage
 {
 
-    public function execute($title)
+    public function execute($title, $identifier)
     {
+        $connexion = new DatabaseConnection();
+        
         $nav = new Active();
         $navig = $nav->getActive($title);
+
+        $postRepository = new LastPosts();
+        $postRepository->connection = $connexion;
+        $post = $postRepository->lastPosts();
 
         require('templates/homepage.php');
     }
